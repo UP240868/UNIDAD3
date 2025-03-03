07aac1f8d37118d58cb4709778592a134310cbda
UP240868 <up240868@alumnos.upa.edu.mx>
2025-03-03 14:44:33 -0600

    Add files via upload

---
+++ 01_day exercises.py
@@ -0,0 +1,14 @@
+2
+print(4 + 5)
+print(8 - 2)
+print(6 * 2)
+print(8 / 4)
+print(6 ** 3)
+print(5 % 3)
+print(5 // 9)
+
+3
+print('Name: Mariana ')
+print('Family name: Olivo Cortes')
+print('Contry: Mexico')
+print('I am having a blast learning python')
